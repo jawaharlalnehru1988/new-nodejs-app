9024feb1a74e274ba6d7ca9ca6222c5f2587925f
nehru00123@gmail.com <nehru00123@gmail.com>
2025-04-05 15:06:01 +0530

    simple nodejs command

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
